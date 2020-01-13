e83766146a5931bf5b1a5aa56d804991acab6bb5
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-01-13 15:30:21 +0700

    add some file

---
+++ flutter_hello_world/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Hello World',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Hello World'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final String title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Application"),
+      ),
+      body: Center(
+        child: Text(title),
+      ),
+    );
+  }
+}
